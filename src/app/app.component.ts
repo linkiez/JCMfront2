660ef5eacb6c37fb127f28af148d9a00fb208037
Fabio Moreno <linkiez@gmail.com>
2022-08-09 08:39:08 -0300

    login screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
 
+import { MenuItem } from 'primeng/api';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'JCMfront2';
+
+  public menuItems: MenuItem[] = [
+    {
+      label: 'Cadastro',
+      items: [
+        {
+          label: 'Contatos',
+        },
+        {
+          label: 'Fornecedores'
+        },
+        {
+          label: 'Operadores',
+        },
+        {
+          label: 'Orcamentos',
+        },
+        {
+          label: 'Ordens de Produção',
+        },
+        {
+          label: 'Pedidos de Compra',
+        },
+        {
+          label: 'Pessoas',
+        },
+        {
+          label: 'Produtos',
+        },
+        {
+          label: 'Registro de Inspeção e Recebimento',
+        },
+        {
+          label: 'Vendedores',
+        }
+
+      ],
+    },
+    {
+      label: 'Configurações',
+      items: [
+        {
+          label: 'Listas Genericas'
+        },
+        {
+          label: 'Arquivos',
+        },
+        {
+          label: 'Usuarios',
+        }
+      ]
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { LoginComponent } from './screen/login/login.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    MenuModule,
+    MenubarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
