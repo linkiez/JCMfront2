4f8a5856e74a9b1202be85b572d75dd6f46f5461
Fabio Moreno <linkiez@gmail.com>
2023-01-27 15:05:42 -0300

    ordem de producao templete

--- src/app/screen/home/ordens-producao/ordens-producao.module.ts
+++ src/app/screen/home/ordens-producao/ordens-producao.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { OrdensProducaoRoutingModule } from './ordens-producao-routing.module';
 import { OrdensProducaoComponent } from './ordens-producao.component';
 import { OrdemProducaoComponent } from './ordem-producao/ordem-producao.component';
+import { ListaFilesModule } from 'src/app/components/listaFiles/listaFiles.module';
 
 
 @NgModule({
   declarations: [OrdensProducaoComponent, OrdemProducaoComponent],
   imports: [
     CommonModule,
-    OrdensProducaoRoutingModule
+    OrdensProducaoRoutingModule,
+    ListaFilesModule
   ]
 })
 export class OrdensProducaoModule { }
