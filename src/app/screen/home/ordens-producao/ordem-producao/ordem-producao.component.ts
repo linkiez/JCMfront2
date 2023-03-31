20b239272a977274617abb025a7cb31b59f53657
Fabio Moreno <linkiez@gmail.com>
2023-03-31 12:56:27 -0300

    ordem dem producao

--- src/app/screen/home/ordens-producao/ordem-producao/ordem-producao.component.ts
+++ src/app/screen/home/ordens-producao/ordem-producao/ordem-producao.component.ts
@@ -3,33 +3,37 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { OrdemProducao } from 'src/app/models/ordem-producao';
 import { OrdemProducaoService } from 'src/app/services/ordem-producao.service';
+import {Quill} from 'quill';
 
 @Component({
   selector: 'app-ordem-producao',
   templateUrl: './ordem-producao.component.html',
-  styleUrls: ['./ordem-producao.component.css']
+  styleUrls: ['./ordem-producao.component.css'],
 })
 export class OrdemProducaoComponent implements OnInit {
-
-  ordemProducao: OrdemProducao = {}
+  ordemProducao: OrdemProducao = {};
 
   constructor(
     private ordemProducaoService: OrdemProducaoService,
     private route: ActivatedRoute,
     private messageService: MessageService,
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.getOrdemProducao();
   }
 
-  getOrdemProducao(){
+  getOrdemProducao() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.ordemProducaoService.getOrdemProducao(id).subscribe({
       next: (ordemProducao) => {
         console.log(ordemProducao);
-        this.ordemProducao = ordemProducao
+        this.ordemProducao = ordemProducao;
+
+        this.ordemProducao.ordem_producao_items?.forEach((item)=>{
+          item.observacao = '<p>'+item.observacao+'<p>'
+        })
       },
       error: (error) => {
         console.log(error);
@@ -39,15 +43,46 @@ export class OrdemProducaoComponent implements OnInit {
           detail: `${error.status} - ${error.statusText} - ${error.error}`,
         });
       },
-      complete: () => {}
-    })
+      complete: () => {},
+    });
   }
 
   getBack() {
     this.router.navigate(['home/ordemproducao']);
   }
 
-  salvar(){
-    console.log(this.ordemProducao);
+  salvar() {
+    this.ordemProducaoService
+      .updateOrdemProducao(this.ordemProducao)
+      .subscribe({
+        next: (ordemProducao) => {
+          this.ordemProducao = ordemProducao;
+        },
+        error: (error) => {
+          console.log(error);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: `${error.status} - ${error.statusText} - ${error.error}`,
+          });
+        },
+        complete: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Sucesso',
+            detail: 'A ordem de produção foi atualizada.',
+          });
+        },
+      });
+  }
+
+  onEditorModelChange(editor: Quill, content: string, index: number){
+    const contents = editor.clipboard.convert({html: content});
+    editor.setContents(contents);
+  }
+
+  consolelog(content:any){
+    console.log(content)
+    console.log(this.ordemProducao)
   }
 }
