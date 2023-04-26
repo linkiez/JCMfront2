31c914b894c79cf2e42174044186c3ab402b169a
Fabio Moreno <linkiez@gmail.com>
2023-04-26 14:09:41 -0300

    dockerfile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'http://localhost:3000/',
+  backendURL: 'http://192.168.0.2:2486/',
   accessTokenExpirein: 15 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
