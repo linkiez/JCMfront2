f519e4559567a983f5ed0948b8a4e3761f4d80ba
Fabio Moreno <linkiez@gmail.com>
2023-05-27 11:24:46 -0300

    inclusao de dominio em producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'http://localhost:3000/',
+  backendURL: 'http://sistema.jcmmetais.com.br:2486/',
   accessTokenExpirein: 15 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
