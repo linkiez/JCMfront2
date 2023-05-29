4ff10a97aaa38894ec9ce5b8010c44bc374c2be3
Fabio Moreno <linkiez@gmail.com>
2023-05-29 09:06:19 -0300

    sever.js to server.js

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'http://linkiez.ddns.net:2486/',
+  backendURL: 'https://jcm-backend.herokuapp.com/',
   accessTokenExpirein: 15 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
