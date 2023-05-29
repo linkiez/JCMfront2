fa2cee87d3b338ce75d609779df96769ca2436a1
Fabio Moreno <linkiez@gmail.com>
2023-05-29 08:51:11 -0300

    change package

--- sever.js
+++ sever.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
-
-app.use(express.static('./dist/jcmfront2'));
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/jcmfront2/'});
+app.use(express.static(__dirname + "/dist/jcmfront2"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/jcmfront2/index.html"));
 });
-
 app.listen(process.env.PORT || 8080);
