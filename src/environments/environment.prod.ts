6ba37f687501020af5ab0aaf75d40725ff1371a9
Fabio Moreno <linkiez@gmail.com>
2023-06-02 09:08:11 -0300

    change backendURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'http://192.168.0.2:2486/',
+  backendURL: 'https://jcm-backend.herokuapp.com/',
   accessTokenExpirein: 15 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
