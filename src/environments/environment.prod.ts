8e2d45529c3d1680d7c6641e4e658f127b706c7f
LiNkIeZ <linkiez@gmail.com>
2023-07-24 09:19:08 -0300

    add : to url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: location.protocol === 'https' ? 'https://linkiez.ddns.net:2487/' : 'http://linkiez.ddns.net:2486/',
+  backendURL: location.protocol === 'https:' ? 'https://linkiez.ddns.net:2487/' : 'http://linkiez.ddns.net:2486/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
