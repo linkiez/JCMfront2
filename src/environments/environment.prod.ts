39b4d6c3dde351cebb4ef32bbcf79aff5e9d0d36
LiNkIeZ <linkiez@gmail.com>
2023-09-04 15:42:37 -0300

    Revert "change backend url"
    This reverts commit 1a4d8ecb6c0117139c87ab6303bbafb101f4d175.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'http://linkiez.ddns.net:2486/',
+  backendURL: 'https://linkiez.ddns.net:2487/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
