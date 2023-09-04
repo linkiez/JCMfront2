1a4d8ecb6c0117139c87ab6303bbafb101f4d175
LiNkIeZ <linkiez@gmail.com>
2023-09-04 15:30:21 -0300

    change backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'https://linkiez.ddns.net:2487/',
+  backendURL: 'http://linkiez.ddns.net:2486/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
