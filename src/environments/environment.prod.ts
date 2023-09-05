34ccea001e1e155401d70d224db7a67f4aa408e7
LiNkIeZ <linkiez@gmail.com>
2023-09-05 08:33:50 -0300

    Revert "add backend api url env"
    This reverts commit 006f544740ed04d28d1fd1fef4093332b5891e65.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: process.env["BACKEND_URL"] || 'http://localhost:3000/',
+  backendURL: 'https://linkiez.ddns.net:2487/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
