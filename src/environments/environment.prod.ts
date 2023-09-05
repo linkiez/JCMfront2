006f544740ed04d28d1fd1fef4093332b5891e65
LiNkIeZ <linkiez@gmail.com>
2023-09-05 08:25:48 -0300

    add backend api url env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL: 'https://linkiez.ddns.net:2487/',
+  backendURL: process.env["BACKEND_URL"] || 'http://localhost:3000/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
