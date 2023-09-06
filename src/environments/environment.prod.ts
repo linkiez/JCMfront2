652969bf2f5885074497f527c256035eb68f3ba3
LiNkIeZ <linkiez@gmail.com>
2023-09-06 14:38:27 -0300

    fix backendURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // backendURL: 'https://api.jcmmetais.com.br/',
-  backendURL: 'http://localhost:3000/',
+  backendURL: 'https://api.jcmmetais.com.br/',
+  // backendURL: 'http://localhost:3000/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
