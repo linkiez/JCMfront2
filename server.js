bfc86d42f91bdc6f57f3c5d32b1433e179e4f771
LiNkIeZ <linkiez@gmail.com>
2023-09-07 13:47:59 -0300

    change on server.js

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/jcmfront2"));
+app.get("/Robots.txt", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/jcmfront2/robots.txt"));
+});
+app.get("/robots.txt", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/jcmfront2/robots.txt"));
+});
+app.get("/sitemap.xml", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/jcmfront2/sitemap.xml"));
+});
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/jcmfront2/index.html"));
 });
