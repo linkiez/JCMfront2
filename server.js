82819451bb65b676392bece1d1cab03a20de00ad
LiNkIeZ <linkiez@gmail.com>
2023-09-07 14:07:44 -0300

    revert server.js

--- server.js
+++ server.js
@@ -4,19 +4,14 @@ const path = require("path");
 const app = express();
 const staticFilesDir = path.join(__dirname, "dist", "jcmfront2");
 
-const sitemap = app.use(express.static(staticFilesDir));
+app.use(express.static(staticFilesDir));
 
 app.get("/robots.txt", function (req, res) {
-  res.send(`User-agent: *
-  Disallow: /login
-  Disallow: /home
-  `);
+  res.sendFile(path.join(staticFilesDir, "robots.txt"));
 });
 
 app.get("/sitemap.xml", function (req, res) {
-  res.send(
-    '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1"><url><loc>https://www.jcmmetais.com.br/</loc><changefreq>daily</changefreq><priority>1.0</priority></url><url><loc>https://www.jcmmetais.com.br/produtos</loc><changefreq>weekly</changefreq><priority>0.8</priority></url><url><loc>https://www.jcmmetais.com.br/servicos</loc><changefreq>weekly</changefreq><priority>0.8</priority></url><url><loc>https://www.jcmmetais.com.br/trabalhe</loc><changefreq>weekly</changefreq><priority>0.8</priority></url><url><loc>https://www.jcmmetais.com.br/contato</loc><changefreq>weekly</changefreq><priority>0.8</priority></url></urlset>'
-  );
+  res.sendFile(path.join(staticFilesDir, "sitemap.xml"));
 });
 
 app.get("/*", function (req, res) {
