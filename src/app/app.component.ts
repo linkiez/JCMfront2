c42c1a9a7b665d72d3b3290382ce575651244ee0
Fabio Moreno <linkiez@gmail.com>
2023-09-08 13:38:42 -0300

    Google Tag Manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
-import { MenuItem } from 'primeng/api';
+declare var dataLayer: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JCMfront2';
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Push to Google Tag Manager data layer on route change
+        dataLayer.push({
+          event: 'ngRouteChange',
+          attributes: {
+            route: event.urlAfterRedirects,
+          },
+        });
+      }
+    });
+  }
 }
--- src/app/screen/site/components/card/card.component.ts
+++ src/app/screen/site/components/card/card.component.ts
@@ -10,18 +10,11 @@ import {
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  animations: [
-    trigger('bounceInAnimation', [
-      state('in', style({ opacity: 1 })),
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('5s', style({ opacity: 1 })),
-      ]),
-    ]),
-  ],
+
 })
 export class CardComponent implements OnInit {
   @Input() data: any;
+  @Input() lado: number = 0;
 
   constructor() {}
 
