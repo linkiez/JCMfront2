2f6cd9ad5d5e2ac884380714f594fa3ffac8e5ea
Fabio Moreno <linkiez@gmail.com>
2023-09-08 14:14:51 -0300

    dynamics change title on route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 declare var dataLayer: any;
 
@@ -8,21 +10,28 @@ declare var dataLayer: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'JCMfront2';
-  constructor(private router: Router) {}
+export class AppComponent  {
 
-  ngOnInit() {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        // Push to Google Tag Manager data layer on route change
-        dataLayer.push({
-          event: 'ngRouteChange',
-          attributes: {
-            route: event.urlAfterRedirects,
-          },
-        });
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.changeTitleOnNavigation();
+  }
+
+  private changeTitleOnNavigation(): void {
+    this.router.events.pipe(
+      // Filter out only NavigationEnd events
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      let route = this.activatedRoute;
+      while (route.firstChild) {
+        route = route.firstChild;
       }
+      // Retrieve the current route's title and set it
+      const pageTitle = `${route.snapshot.data['title']?route.snapshot.data['title'] + " | ":""}JCM Metais | Corte e Dobra de Chapas | Americana, SP`;
+      this.titleService.setTitle(pageTitle);
     });
   }
 }
--- src/app/screen/site/site-routing.module.ts
+++ src/app/screen/site/site-routing.module.ts
@@ -3,25 +3,52 @@ import { RouterModule, Routes } from '@angular/router';
 import { SiteComponent } from './site.component';
 
 const routes: Routes = [
-  { path: '', component: SiteComponent, children: [
-    { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-    {
-      path: 'produtos', loadChildren: () => import('./pages/produtos/produtos.module').then(m => m.ProdutosModule)
-    },
-    {
-      path: 'servicos', loadChildren: () => import('./pages/servicos/servicos.module').then(m => m.ServicosModule)
-    },
-    {
-      path: 'trabalhe', loadChildren: () => import('./pages/trabalhe/trabalhe.module').then(m => m.TrabalheModule)
-    },
-    {
-      path: 'contato', loadChildren: () => import('./pages/contato/contato.module').then(m => m.ContatoModule)
-    }
-  ] }
+  {
+    path: '',
+    component: SiteComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./pages/home/home.module').then((m) => m.HomeModule),
+        data: { title: 'Home Page' },
+      },
+      {
+        path: 'produtos',
+        loadChildren: () =>
+          import('./pages/produtos/produtos.module').then(
+            (m) => m.ProdutosModule
+          ),
+        data: { title: 'Produtos' },
+      },
+      {
+        path: 'servicos',
+        loadChildren: () =>
+          import('./pages/servicos/servicos.module').then(
+            (m) => m.ServicosModule
+          ),
+        data: { title: 'Serviços' },
+      },
+      {
+        path: 'trabalhe',
+        loadChildren: () =>
+          import('./pages/trabalhe/trabalhe.module').then(
+            (m) => m.TrabalheModule
+          ),
+        data: { title: 'Trabalhe Conosco' },
+      },
+      {
+        path: 'contato',
+        loadChildren: () =>
+          import('./pages/contato/contato.module').then((m) => m.ContatoModule),
+        data: { title: 'Contato' },
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SiteRoutingModule { }
+export class SiteRoutingModule {}
