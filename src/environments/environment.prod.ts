ee3c6e5e5db32aa43dbd9fdb24c2edbbbb979b25
LiNkIeZ <linkiez@gmail.com>
2024-03-12 09:09:40 -0300

    Icons Fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   // production: false,
   backendURL: 'https://api.jcmmetais.com.br/',
+  // backendURL: 'http://jcmmetais.ddns.net:57338/',
   // backendURL: 'http://localhost:3000/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
