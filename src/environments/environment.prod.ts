fe73b2c20ecd08ab4fdfa82a6b6dc9863a90f608
LiNkIeZ <linkiez@gmail.com>
2024-03-29 17:41:32 -0300

    change backendURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // production: false,
   // backendURL: 'https://api.jcmmetais.com.br/',
-  backendURL: 'https://jcmmetais.ddns.net:57339/',
+  backendURL: 'https://jcmmetais.ddns.net/api/',
   // backendURL: 'http://localhost:3000/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
