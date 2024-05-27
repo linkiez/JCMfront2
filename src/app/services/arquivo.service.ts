4b3c78e59e7bbbf3c2a3ee543a113e73cffa679c
LiNkIeZ <linkiez@gmail.com>
2024-05-27 10:37:34 -0300

    #68 Subject: Enhance file handling and zip downloads
    - Upgraded project version from 1.13.1 to 1.13.5 in package-lock.json and added dependencies for file handling (`file-saver` and `jszip`) along with their types to improve file download capabilities.
    - Implemented functionality to download all files as a zip in both `orcamento` and `ordem-producao` components, enabling efficient bulk file downloads.
    - Modified the `arquivo.service.ts` to support the new zip download feature by adding methods to fetch file URLs, download files as blobs, and zip these files for the client-side download.
    - Replaced the `File[]` type with `IArquivo[]` in the `orcamento` model to align with backend changes and to facilitate enhanced file manipulation features.
    - Minor UI adjustments for consistency and to accommodate new download button for zipped files.
    - Added a utility function `consoleLogDev` to streamline conditional logging based on the environment.
    
    This update significantly enhances the user experience by simplifying the process of downloading multiple files and aligns file handling features with backend changes. Refactoring for clarity and maintainability also forms a part of this commit.
    
    References: #13456, #13579

--- src/app/services/arquivo.service.ts
+++ src/app/services/arquivo.service.ts
@@ -1,9 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, firstValueFrom, forkJoin, map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IArquivo } from '../models/arquivo';
-
+import { IPedidoCompraItem } from '../models/pedido-compra';
+import { IOrcamentoItem } from '../models/orcamento';
+import { IOrdemProducaoItem } from '../models/ordem-producao';
+import JSZip from 'jszip';
+import * as FileSaver from 'file-saver';
 @Injectable({
   providedIn: 'root',
 })
@@ -48,5 +52,47 @@ export class ArquivoService {
     return this.http.get<string>(environment.backendURL + 'file/url/' + id, {
       responseType: 'json',
     });
+  }
+
+  async downloadAllFilesFromArray(
+    array: IOrdemProducaoItem[] | IOrcamentoItem[]
+  ) {
+    const files = array.map((item) => item.files ?? []).flat();
+    let filesUrl = files.map((file) =>
+      firstValueFrom(this.getUrlArquivo(file?.id!))
+    );
+
+    const blobs$ = (await Promise.all(filesUrl)).map((url, index) =>
+      this.downloadArquivo(url).pipe(
+        map((blob) => ({ fileName: files[index].originalFilename!, blob })),
+      )
+    );
+
+    forkJoin(blobs$).pipe(
+      map(files => files.filter(file => file !== null)), // remove null values
+      map(files => this.zipFiles(files))
+    )
+    .subscribe(zipBlob => {
+        zipBlob.then((zipBlob) => {
+          FileSaver.saveAs(zipBlob, 'files.zip');
+        });
+
+    });
+  }
+
+  private downloadArquivo(url: string): Observable<Blob> {
+    return this.http.get(url, {
+      responseType: 'blob'
+    });
+  }
+
+  private async zipFiles(files: { fileName: string; blob: Blob }[]) {
+    const zip = new JSZip();
+
+    files.forEach(file => {
+      zip.file(file.fileName, file.blob);
+    });
+
+    return await zip.generateAsync({ type: 'blob' });
   }
 }
