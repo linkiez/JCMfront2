325489ccc31e33a1eb7743fe6694821c12192e57
LiNkIeZ <linkiez@gmail.com>
2024-06-29 14:29:18 -0300

    chore(environment): update backend URL for production
    Updated the backendURL in the production environment configuration to point to the new server address. This change ensures that the application communicates with the correct backend service in the production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // production: false,
-  backendURL: 'http://192.168.80.200:2486/',
+  backendURL: 'http://refilauq.n4sti.com.br:2486/',
   accessTokenExpirein: 480 /*in minutes*/,
   refreshTokenExpireIn: 5 /*in days*/
 };
