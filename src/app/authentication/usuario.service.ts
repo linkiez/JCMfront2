94438e4790a2b747ba0bcdb9be0888ceadc4c1a7
LiNkIeZ <linkiez@gmail.com>
2024-07-05 14:58:36 -0300

    feat(authentication): implement reactive user state management
    Added BehaviorSubject to UsuarioService for managing user state reactively. Updated components to subscribe to user state changes and handle cleanup on destroy. This improves the synchronization of user data across the application and ensures proper resource management by unsubscribing from observables when components are destroyed.

--- src/app/authentication/usuario.service.ts
+++ src/app/authentication/usuario.service.ts
@@ -1,8 +1,8 @@
-import { firstValueFrom } from 'rxjs';
+import { BehaviorSubject, firstValueFrom, Observable, Subscription } from 'rxjs';
 import { UsuarioServiceDB } from 'src/app/services/usuarioDB.service';
 import { jwtDecode } from 'jwt-decode';
 import { AuthenticationService } from './authentication.service';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { AccessTokenService } from './accessToken.service';
 import { RefreshTokenService } from './refreshToken.service';
 import { IUsuario } from '../models/usuario';
@@ -10,8 +10,11 @@ import { IUsuario } from '../models/usuario';
 @Injectable({
   providedIn: 'root',
 })
-export class UsuarioService {
-  private usuario: IUsuario = null;
+export class UsuarioService implements OnInit, OnDestroy {
+  userBehaviorSubject: BehaviorSubject<IUsuario> = new BehaviorSubject<IUsuario>(null);
+  user$: Observable<IUsuario> = this.userBehaviorSubject.asObservable();
+  userSubscription: Subscription;
+  usuario: IUsuario = null;
 
   constructor(
     private accessTokenService: AccessTokenService,
@@ -24,15 +27,29 @@ export class UsuarioService {
     }
   }
 
+  ngOnInit(): void {
+    this.userSubscription = this.user$.subscribe((usuario) => {
+      this.usuario = usuario;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+
   private async decodificaJWT() {
     const token = this.accessTokenService.retornaToken();
     const { id } = jwtDecode(token) as { id: number };
-    if(!this.usuario)
-    this.usuario = await firstValueFrom(this.usuarioServiceDB.getUsuario(id))
+    if (!this.usuario) {
+      const usuario = await firstValueFrom(
+        this.usuarioServiceDB.getUsuario(id)
+      );
+      this.userBehaviorSubject.next(usuario);
+    }
   }
 
-  getUsuario() {
-    return this.usuario;
+  getUsuario$() {
+    return this.user$;
   }
 
   async salvaToken(accesstoken: string, refreshToken: string) {
@@ -49,6 +66,6 @@ export class UsuarioService {
     this.authenticationService.logout();
     this.accessTokenService.excluiToken();
     this.refreshTokenService.excluiToken();
-    this.usuario = null;
+    this.userBehaviorSubject.next(null);
   }
 }
